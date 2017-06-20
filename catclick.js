8ec849d01b56348ff4cf0900750cb1c09ddb7335
Matt <mattbledsoe2@gmail.com>
2017-06-20 16:57:34 -0400

    initial commit

---
+++ catclick.js
@@ -0,0 +1,10 @@
+
+var count = 0
+
+document.getElementById("siam").addEventListener("click", catCount);
+
+function catCount() {
+    document.getElementById("counter").innerHTML = date();
+}
+
+};
