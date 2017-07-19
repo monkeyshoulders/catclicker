64fec26557e07fa028a3b9e11c3e01139ac9be9e
Matt <mattbledsoe2@gmail.com>
2017-07-19 11:23:38 -0400

    changed file path for images

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@ var ViewModel = function () {
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.level = ko.observable('infant')
-  this.imgSrc = ko.observable('img/2.jpg');
+  this.imgSrc = ko.observable('img/2.gif');
 
   this.incrementCounter = function() {
     this.clickCount(this.clickCount()+ 1);
   };
-  
+
 
 }
 
