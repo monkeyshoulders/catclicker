1c452fedba6ba6472dd3dd3a29835f4fa99d8ead
Matt <mattbledsoe2@gmail.com>
2017-07-19 11:18:42 -0400

    changing app with knockoutJS

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var ViewModel = function () {
+  this.clickCount = ko.observable(0);
+  this.name = ko.observable('Tabby');
+  this.level = ko.observable('infant')
+  this.imgSrc = ko.observable('img/2.jpg');
+
+  this.incrementCounter = function() {
+    this.clickCount(this.clickCount()+ 1);
+  };
+  
+
+}
+
+ko.applyBindings(new ViewModel())
